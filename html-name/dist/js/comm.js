9117b75e01d3e3d52886597ee100c77f917fa749
Imseesea <136055408+Imseesea@users.noreply.github.com>
2023-08-03 17:04:23 +0900

    goods-list-script

--- html-name/dist/js/comm.js
+++ html-name/dist/js/comm.js
@@ -1,13 +1,20 @@
-var swiper = new Swiper(".slide-intro", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+// var swiper = new Swiper(".slide-intro", {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+$(function () {
+  $(".goods-lst__els").removeClass("active");
+  $(".goods-lst__trigger").on("click", function () {
+    $(".goods-lst__els").toggleClass("active");
   });
+});
